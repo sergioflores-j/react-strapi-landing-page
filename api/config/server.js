d1d2a5682d101f31f0477d381d3574508d63a439
Julien Bisconti <julienbi@unity3d.com>
2020-07-28 20:15:53 -0300

    feat: add strapi api

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef37053c2543df36dd2cb5e26ad016c8'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e1664870-6634-4950-947a-3884b21fb629'
+};
