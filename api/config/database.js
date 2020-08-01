2119533d89619d0dd3ce12e1177ae240f68c716e
Sergio Eduardo Flores Junior <sergio.flores.junior17@gmail.com>
2020-07-31 22:00:54 -0300

    change to deploy heroku app

--- api/config/database.js
+++ api/config/database.js
@@ -12,7 +12,9 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', 'strapi123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {}
+      options: {
+
+      }
     },
   },
 });
