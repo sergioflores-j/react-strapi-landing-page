33e1a7cb936a887df2aba0f667eebe169951db52
Sergio Eduardo Flores Junior <sergio.flores.junior17@gmail.com>
2020-08-20 23:11:09 -0300

    add netlify webhook

--- api/api/landing-page/models/landing-page.js
+++ api/api/landing-page/models/landing-page.js
@@ -5,4 +5,17 @@
  * to customize this model
  */
 
-module.exports = {};
+const axios = require('axios');
+const netlifyWebhook = strapi.config.get('custom.netlifyWebhook');
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebhook && await axios.post(netlifyWebhook);
+    },
+
+    async afterUpdate(_, __, ___) {
+      netlifyWebhook && await axios.post(netlifyWebhook);
+    },
+  }
+};
